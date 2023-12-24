964a73b76e9a0e2b641c0d1157ce1c8364a54f8e
Myiea <110724590+Myiea@users.noreply.github.com>
2023-12-24 23:45:17 +0700

    update exercise 1, working but not really

--- week 8/exercise1.cpp
+++ week 8/exercise1.cpp
@@ -1,45 +1,68 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class Node{
     private:
         int value;
         Node *next;
-        Node *previous;
+        Node *prev;
 
     public:
-        Node(int value, Node *next, Node *previous){}
+        Node(int value): value(value), next(nullptr), prev(nullptr) {}
 
-        void linkForward(Node *nextNode){
-            this -> next = nextNode;
-
-            if (nextNode){
-
-            }
+        void linkedNextNode(Node *nextNode){
+            next = nextNode;
+            this -> prev = nextNode;
         }
 
-        void insertNode(Node *&head, Node *&tail, Node *priorNode, Node *newNode){
-            if(priorNode == nullptr){
-                newNode -> linkForward(head);
-                head = newNode;
-            }else if (priorNode == tail){
-                tail -> linkForward(newNode);
-                tail = newNode;
-            }else{
-                newNode -> linkForward(priorNode -> next);
-                priorNode = newNode;
-            }
+        void linkedPrevNode(Node *prevNode){
+            prev = prevNode;
+            prevNode -> next = this;
         }
 
-        void showForward(Node *head){
-            Node *temp = head;
-            cout << "Forward connection: \n";
-            while(temp != nullptr){
-                cout << temp->value << "-->";
-                temp = temp->next;
-            }
-            cout << endl;
-        }
+        friend void showForthLinks(Node *head);
+        friend void showPrevLinks(Node *tail);
 };
 
-int main(){}
+// Function to show links
+void showForthLinks(Node *head){
+    cout << "Forward links: ";
+    Node *temp = head;
+    while(temp != nullptr){
+        cout << setw(3) << temp -> value << "--->";
+        temp = temp -> next;
+    } 
+    cout << endl;
+}
+
+void showPrevLinks(Node *tail){
+    cout << "Backward links: ";
+    Node *temp = tail;
+    while(temp != nullptr){
+        cout << setw(3) << temp -> value << "--->";
+        temp = temp -> prev;
+    } 
+    cout << endl;
+}
+
+void showLinks(Node *head, Node *tail){
+    showPrevLinks(tail);
+    showForthLinks(head);
+}
+
+int main(){
+    //Testing code
+    Node node1(8), node2(5), node3(3), node4(6);
+
+    Node *head = &node1; // pointer variable
+    node1.linkedNextNode(&node2);
+    node2.linkedNextNode(&node3);
+    node3.linkedNextNode(&node4);
+
+    Node *tail = &node4; // pointer variable
+
+    // show connection
+    cout << "Original linked list: \n";
+    showLinks(head, tail);
+}
